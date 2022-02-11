a918fe5da8a96c3a670d64091f8443b9a749c759
indhumathi <Indhumathi.Ramesh@disys.com>
2022-02-11 07:09:49 -0800

    Add project files.

---
+++ Run time polymorphism/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+public class EmployeeOne
+{
+    public virtual void salary()
+    {
+        Console.WriteLine("EmployeeOne salary is 50,000");
+    }
+}
+public class EmployeeTwo : EmployeeOne
+{
+    public override void salary()
+    {
+        base.salary();
+        Console.WriteLine("EmployeeTwo salary is 60,000");
+    }
+}
+public class TestOverriding
+{
+    public static void Main(string[] args)
+    {
+        EmployeeTwo e = new EmployeeTwo();
+        e.salary();
+    }
+}
